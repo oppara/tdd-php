6a63e0dda0fd129be2feea772ca2f788c58e0640
oppara <oppara@oppara.tv>
2018-04-15 18:27:20 +0900

    Implement chapter05

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -4,6 +4,7 @@ namespace Money\Test;
 
 use PHPUnit\Framework\TestCase;
 use Money\Doller;
+use Money\Franc;
 
 
 class MoneyTest extends TestCase
@@ -15,11 +16,17 @@ class MoneyTest extends TestCase
         $this->assertEquals(new Doller(15), $five->times(3));
     }
 
-
     public function testequality()
     {
         $this->assertTrue((new Doller(3))->equals(new Doller(3)));
         $this->assertFalse((new Doller(5))->equals(new Doller(6)));
+    }
+
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertEquals(new Franc(10), $five->times(2));
+        $this->assertEquals(new Franc(15), $five->times(3));
     }
 }
 
