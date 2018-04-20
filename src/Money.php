5e4e01feb495899a835ab581b6a69a192d2f7ce5
oppara <oppara@oppara.tv>
2018-04-21 01:18:05 +0900

    Implemnt chapter09

--- src/Money.php
+++ src/Money.php
@@ -4,6 +4,13 @@ namespace Money;
 abstract class Money
 {
     protected $amount;
+    protected $currency;
+
+    public function __construct(int $amount, string $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
 
     public function equals(Money $money) : bool
     {
@@ -13,14 +20,19 @@ abstract class Money
 
     public abstract function times(int $multiplier) : Money;
 
+    public function currency() : String
+    {
+        return $this->currency;
+    }
+
     public static function doller(int $amount) : Money
     {
-        return new Doller($amount);
+        return new Doller($amount, 'USD');
     }
 
     public static function franc(int $amount) : Money
     {
-        return new Franc($amount);
+        return new Franc($amount, 'CHF');
     }
 }
 
